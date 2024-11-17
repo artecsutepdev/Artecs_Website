868f85e508bdd333942e3465f234a51da815d0fe
Rafael Garcia <121538800+unmemero@users.noreply.github.com>
2024-11-17 12:44:15 -0700

    Delete scripts directory

--- scripts/script.js
+++
@@ -1,7 +0,0 @@
-const hamburger = document.querySelector('.hamburger-container');
-const nav = document.querySelector('nav');
-
-hamburger.addEventListener('click', function() {
-    this.classList.toggle('open');
-    nav.classList.toggle('open');
-});
